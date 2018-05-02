d7822309be10f229920861bdc388d75200a6f916
Ivan Zugnoni <zugnoni.ivan@gmail.com>
2018-05-01 23:03:30 -0300

    Remove fields from admin

--- django_practice_2/artists/admin.py
+++ django_practice_2/artists/admin.py
@@ -5,11 +5,4 @@ from .models import Artist, Song
 
 @admin.register(Artist)
 class ArtistAdmin(admin.ModelAdmin):
-    list_display = ('id', 'artistic_name', 'first_name', 'last_name', 'genre',)
-    list_filter = ('genre',)
-
-
-@admin.register(Song)
-class SongAdmin(admin.ModelAdmin):
-    list_display = ('id', 'artist_id', 'title', 'album_name')
-    list_filter = ('album_name',)
+    list_display = ('id', 'artistic_name', 'first_name', 'last_name',)
