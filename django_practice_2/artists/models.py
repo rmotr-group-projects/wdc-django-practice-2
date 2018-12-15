9fe4316a06657bc05630ebd305b3565d0921b5df
mikoumed <mikoumed87@gmail.com>
2018-12-15 13:28:49 +0000

    get all tests passing

--- django_practice_2/artists/models.py
+++ django_practice_2/artists/models.py
@@ -18,15 +18,17 @@ class Artist(models.Model):
     artistic_name = models.CharField(max_length=255)
     picture_url = models.URLField()
     popularity = models.IntegerField()
-    # genre = ...
+    genre = models.CharField(max_length=255,choices=GENRE_CHOICES)
 
 
-# class Song(models.Model):
-#     """
-#         PART 3:
-#             - Task 1: Implement the Song model with the following fields:
-#                 * artist_id (type: integer)
-#                 * title (type: char)
-#                 * album_name (type: char)
-#     """
-#     pass
+class Song(models.Model):
+    """
+        PART 3:
+            - Task 1: Implement the Song model with the following fields:
+                * artist_id (type: integer)
+                * title (type: char)
+                * album_name (type: char)
+    """
+    artist_id = models.IntegerField()
+    title = models.CharField(max_length=255)
+    album_name = models.CharField(max_length=255)
--- django_practice_2/artists/orm_exercises.py
+++ django_practice_2/artists/orm_exercises.py
@@ -3,34 +3,42 @@ from artists.models import Artist, Song
 
 def task_1_artists_filter_by_popularity():
     """Should return all artists that have more than 80 popularity"""
-    pass
-
+    artists = Artist.objects.filter(popularity__gt=80)
+    return artists
 
 def task_2_artists_get_by_artistic_name():
     """Should return the artist which artistic name is Jimi Hendrix"""
-    pass
-
+    artist = Artist.objects.get(artistic_name='Jimi Hendrix')
+    return artist
 
 def task_3_songs_delete():
     """Should delete all songs that contain any letter 'a' in its title"""
-    pass
+    Song.objects.filter(title__icontains='a').delete()
 
 
 def task_4_artists_create_song():
     """Should create a new song for Ed Sheeran artist"""
-    pass
-
+    artist = Artist.objects.get(artistic_name='Ed Sheeran')
+    Song.objects.create(
+        artist_id=artist.id,
+        title='new song',
+        album_name='album'
+    )
 
 def task_5_artists_order_by_popularity():
     """Should return all artists ordered by popularity"""
-    pass
+    artists = Artist.objects.all().order_by('popularity')
+    return artists
 
 
 def task_6_song_edit_album():
     """Should take the song with title 'Superstition' and update its album name with any other name"""
-    pass
-
+    song = Song.objects.get(title='Superstition')
+    song.album_name = 'Other name'
+    song.save()
+    
 
 def task_7_song_counter():
     """Should return the amount of songs stored in the database"""
-    pass
+    count_songs = Song.objects.all().count()
+    return count_songs
